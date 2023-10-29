7437a8ce0675b7e0522f4fd30b644b72e5da8f53
Gulshan <gulshansinghhhh@gmail.com>
2023-10-29 11:40:58 +0530

    changed theme

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// ------------------------------APP----------------------------------//
+
+
+import React from "react";
+
+import Hero from "./Components/Hero";
+import Nav from "./Components/Nav";
+import Moto from "./Components/Moto";
+import Initia from "./Components/Initia";
+
+
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+      <Moto />
+      <Initia />
+     
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
